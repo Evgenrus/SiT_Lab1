06d3980081555964ef9819627f0dccc7253afa11
Evgen <evgenrus257@yandex.ru>
2021-10-03 23:27:18 +0700

    tests

--- udp/test_udp.py
+++ udp/test_udp.py
@@ -20,6 +20,9 @@ class TestUDP(unittest.TestCase):
     def test_G1(self):
         self.assertEqual(G1('каракатица'), 'кариакатица')
 
+    def test_generate_msg(self):
+        self.assertTrue(type(msg_generator()) == str)
+
     def test_m_recvfrom(self):
         host_1 = ''
         port_1 = 2002
@@ -31,26 +34,6 @@ class TestUDP(unittest.TestCase):
         msg_1, addr_1 = m_recvfrom(sock_1)
         sock_1.close()
         self.assertNotEqual(msg_1, 'err')
-
-    def test_client(self):
-        host = 'localhost'
-        port1 = 2001
-        addr1 = (host, port1)
-        sock1 = socket(AF_INET, SOCK_DGRAM)
-        sock1.bind(addr1)
-
-        port2 = 2002
-        addr2 = (host, port2)
-        sock2 = socket(AF_INET, SOCK_DGRAM)
-        sock2.bind(addr2)
-
-        os.startfile(r'applications\client\client.py')
-        msg1, addr1 = m_recvfrom(sock1)
-        msg2, addr2 = m_recvfrom(sock2)
-        sock1.close()
-        sock2.close()
-
-        self.assertTrue((msg1 and msg2))
 
 
 if __name__ == '__main__':
